1fb701986ea8841325b1785dc0341af54fc08ce6
martlin2cc <martlin2cc@martlin.cz>
2025-08-29 17:04:42 +0200

    Initial main actually syncing

--- pyebasy/main.py
+++ pyebasy/main.py
@@ -1,2 +1,24 @@
+import sys
 
-print("Magic will happen here")
+import pathlib
+
+from cache_sqlite import SqliteCache
+from storage_filesystem import DefaultFileSystemStorage
+from synchronizer_impls import DefaultSynchronizer
+
+source_cache = SqliteCache(pathlib.Path('source.db'))
+destination_cache = SqliteCache(pathlib.Path('destination.db'))
+
+synchronizer = DefaultSynchronizer(
+    source_cache=source_cache,
+    destination_cache=destination_cache,
+    update_source_cache=True,
+    update_destination_cache=True)
+
+source_top_path = pathlib.Path(sys.argv[1])
+source_storage = DefaultFileSystemStorage(source_top_path)
+
+destination_top_path = pathlib.Path(sys.argv[2])
+destination_storage = DefaultFileSystemStorage(destination_top_path)
+
+synchronizer.synchronize(source_storage, destination_storage)
